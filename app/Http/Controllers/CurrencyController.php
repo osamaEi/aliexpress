fe393c7f96c69d798f3f4b956a9b9ed4c59713b0
osama bakry <o.bakry@loop.sa>
2025-12-31 09:25:00 +0200

    done

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -18,6 +18,24 @@ class CurrencyController extends Controller
             session(['currency_code' => $currency->code]);
         }
 
+        // Get the previous URL and update currency parameter if exists
+        $previousUrl = url()->previous();
+        $parsedUrl = parse_url($previousUrl);
+
+        if (isset($parsedUrl['query'])) {
+            parse_str($parsedUrl['query'], $queryParams);
+
+            // Update currency in query params if it exists
+            if (isset($queryParams['currency'])) {
+                $queryParams['currency'] = $currency ? $currency->code : $code;
+                $newQuery = http_build_query($queryParams);
+                $newUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] .
+                         (isset($parsedUrl['port']) ? ':' . $parsedUrl['port'] : '') .
+                         ($parsedUrl['path'] ?? '') . '?' . $newQuery;
+                return redirect($newUrl);
+            }
+        }
+
         return redirect()->back();
     }
 }
