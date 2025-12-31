5e74438a06203c680aa213223fde75b852f8d80f
osama bakry <o.bakry@loop.sa>
2025-12-31 09:18:09 +0200

    done

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -537,12 +537,35 @@
                                     </p>
 
                                     <!-- Price -->
+                                    @php
+                                        $currentCurrency = request('currency', 'AED');
+                                        $isSAR = $currentCurrency === 'SAR';
+                                        $isAED = $currentCurrency === 'AED';
+                                        $currencySymbols = [
+                                            'SAR' => '',
+                                            'AED' => '',
+                                            'USD' => '$',
+                                            'EUR' => '€',
+                                            'GBP' => '£',
+                                            'KWD' => 'د.ك',
+                                            'QAR' => 'ر.ق',
+                                            'BHD' => 'د.ب',
+                                            'OMR' => 'ر.ع',
+                                            'EGP' => 'ج.م',
+                                            'JOD' => 'د.أ',
+                                            'LBP' => 'ل.ل',
+                                        ];
+                                        $currencySymbol = $currencySymbols[$currentCurrency] ?? $currentCurrency;
+                                    @endphp
                                     <div class="mb-2">
                                         <h5 class="text-primary mb-0 d-flex align-items-center" style="direction: ltr; justify-content: flex-start;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1124.14 1256.39" fill="currentColor" class="me-1 sar-icon" style="vertical-align: middle;">
-                                                <path d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
-                                                <path d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
-                                            </svg>
+                                            @if($isSAR)
+                                                <x-riyal-icon width="18" height="18" class="me-1" />
+                                            @elseif($isAED)
+                                                <x-dirham-icon width="18" height="18" class="me-1" />
+                                            @else
+                                                <span class="me-1" style="font-size: 0.9rem;">{{ $currencySymbol }}</span>
+                                            @endif
                                             @if($product['sale_price_format'])
                                                 {{ preg_replace('/[A-Z]{3}\s*/', '', $product['sale_price_format']) }}
                                             @else
@@ -551,19 +574,25 @@
                                         </h5>
                                         @if(isset($product['admin_profit']) && $product['admin_profit'] > 0)
                                             <small class="text-success d-block d-flex align-items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 1124.14 1256.39" fill="currentColor" class="me-1 sar-icon" style="vertical-align: middle;">
-                                                    <path d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
-                                                    <path d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
-                                                </svg>
+                                                @if($isSAR)
+                                                    <x-riyal-icon width="12" height="12" class="me-1" />
+                                                @elseif($isAED)
+                                                    <x-dirham-icon width="12" height="12" class="me-1" />
+                                                @else
+                                                    <span class="me-1" style="font-size: 0.7rem;">{{ $currencySymbol }}</span>
+                                                @endif
                                                 {{ app()->getLocale() == 'ar' ? 'تشمل' : 'Includes' }} {{ number_format($product['admin_profit'], 2) }} {{ app()->getLocale() == 'ar' ? 'عمولة' : 'profit' }}
                                             </small>
                                         @endif
                                         @if($product['original_price'] > $product['sale_price'])
                                             <small class="text-muted text-decoration-line-through d-flex align-items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 1124.14 1256.39" fill="currentColor" class="me-1 sar-icon" style="vertical-align: middle;">
-                                                    <path d="M699.62,1113.02h0c-20.06,44.48-33.32,92.75-38.4,143.37l424.51-90.24c20.06-44.47,33.31-92.75,38.4-143.37l-424.51,90.24Z"/>
-                                                    <path d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"/>
-                                                </svg>
+                                                @if($isSAR)
+                                                    <x-riyal-icon width="12" height="12" class="me-1" />
+                                                @elseif($isAED)
+                                                    <x-dirham-icon width="12" height="12" class="me-1" />
+                                                @else
+                                                    <span class="me-1" style="font-size: 0.7rem;">{{ $currencySymbol }}</span>
+                                                @endif
                                                 @if($product['original_price_format'])
                                                     {{ preg_replace('/[A-Z]{3}\s*/', '', $product['original_price_format']) }}
                                                 @else
