9e09c982656b1ec6e91b79d07806390550335f68
osama bakry <o.bakry@loop.sa>
2026-01-24 14:58:15 +0200

    done

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -662,7 +662,7 @@
                             <option value="">{{ app()->getLocale() == 'ar' ? 'اختر العملة' : 'Select Currency' }}</option>
                             @foreach($currencies as $currency)
                                 <option value="{{ $currency->code }}" {{ $currentCurrency == $currency->code ? 'selected' : '' }}>
-                                    {{ $currency->symbol }} {{ $currency->localizedName }} ({{ $currency->code }})
+                                    {{ $currency->symbol }}  ({{ $currency->code }})
                                 </option>
                             @endforeach
                         </select>
